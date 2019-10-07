d9245385895c7b6d1055cc40c5fc8a4af80bf9ae
Ali Abdelfattah <ali.abdelfattah.xa@gmail.com>
2019-10-07 14:35:01 +0200

    WIP:e2e

--- InstabugSample/__e2e__/instabug/invoke-prompt-options.spec.js
+++ InstabugSample/__e2e__/instabug/invoke-prompt-options.spec.js
@@ -1,21 +1,22 @@
 import { getId } from '../utilities/native-utility';
 import { NativeActions as InstabugNativeActions, NativeIds as InstabugNativeIds } from './native-actions';
+import { NativeActions as BugReportingNativeActions, NativeIds as BugReportingNativeIds } from '../bug-reporting/native-actions';
 import { ReactNativeActions } from '../utilities/rn-utility';
 import { DEFAULT_TIMEOUT } from '../utilities/settings';
 
 describe('Invoking Prompt Options', () => {
-  it('should show the app main view', async () => {
-    // RN Sanity
-    await ReactNativeActions.checkRnSanity();
-  });
+  // it('should show the app main view', async () => {
+  //   // RN Sanity
+  //   await ReactNativeActions.checkRnSanity();
+  // });
   
-  it('should show instabug prompt options when calling Instabug.show()', async () => {
-    // Action
-    await ReactNativeActions.tapOnInvokeBtn();
+  // it('should show instabug prompt options when calling Instabug.show()', async () => {
+  //   // Action
+  //   await ReactNativeActions.tapOnInvokeBtn();
     
-    // Expectation
-    await waitFor(element(by.nativeId(getId(InstabugNativeIds.PROMPT_TITLE)))).toBeVisible().withTimeout(DEFAULT_TIMEOUT);
-  });
+  //   // Expectation
+  //   await waitFor(element(by.nativeId(getId(InstabugNativeIds.PROMPT_TITLE)))).toBeVisible().withTimeout(DEFAULT_TIMEOUT);
+  // });
   
   describe('[Restart App]', () => {
     beforeEach(async () => {
@@ -23,24 +24,49 @@ describe('Invoking Prompt Options', () => {
       await device.launchApp({newInstance: true});
     });
     
-    it('should show instabug prompt options when tapping the floating button', async () => {
-      // Prepare
-      await ReactNativeActions.tapOnEnableFloatingBtn();
+    // it('should show instabug prompt options when tapping the floating button', async () => {
+    //   // Prepare
+    //   await ReactNativeActions.tapOnEnableFloatingBtn();
 
-      // Action
-      await InstabugNativeActions.tapOnFloatingBtn();
+    //   // Action
+    //   await InstabugNativeActions.tapOnFloatingBtn();
       
-      // Expectation
-      await waitFor(element(by.nativeId(getId(InstabugNativeIds.PROMPT_TITLE)))).toBeVisible().withTimeout(DEFAULT_TIMEOUT);
-    });
+    //   // Expectation
+    //   await waitFor(element(by.nativeId(getId(InstabugNativeIds.PROMPT_TITLE)))).toBeVisible().withTimeout(DEFAULT_TIMEOUT);
+    // });
 
-    it('[iOS] should show instabug prompt options when shaking the device', async () => {
+    // it('[iOS] should show instabug prompt options when shaking the device', async () => {
+    //   if (device.getPlatform() === 'ios') {
+    //     // Action
+    //     await device.shake();
+
+    //     // Expectation
+    //     await waitFor(element(by.nativeId(getId(InstabugNativeIds.PROMPT_TITLE)))).toBeVisible().withTimeout(DEFAULT_TIMEOUT);
+    //   }
+    // });
+  
+    it('should report a bug', async () => {
+      // RN App Sanity
+      await ReactNativeActions.checkRnSanity();
+
+      // Invoke Prompt Options
+      await ReactNativeActions.tapOnInvokeBtn();
+
+      // Tap on Report a Bug option
+      await InstabugNativeActions.tapOnBugPromptOption();
+      
+      // Fill the Email field
+      await BugReportingNativeActions.fillOnBugPromptOption();
+      
+      // Tap on Send Bug Report button
+      await BugReportingNativeActions.tapOnSendBugReportBtn();
+
+      // Expect a "Thank you" popup
       if (device.getPlatform() === 'ios') {
-        // Action
-        await device.shake();
-
-        // Expectation
-        await waitFor(element(by.nativeId(getId(InstabugNativeIds.PROMPT_TITLE)))).toBeVisible().withTimeout(DEFAULT_TIMEOUT);
+        // No AccessibilityIdentifier. Text not working
+        // await waitFor(element(by.text('Thank you'))).toBeVisible().withTimeout(DEFAULT_TIMEOUT);
+      } else {
+        await waitFor(element(by.nativeId(getId(BugReportingNativeIds.SUCCESS_POPUP_MSG)))).toBeVisible().withTimeout(DEFAULT_TIMEOUT);
       }
     });
   });
